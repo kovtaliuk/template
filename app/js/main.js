b3659383bb8a8aea7e3ce57d65476ba5697c698b
kovtaliuk <kovtaliuk@gmail.com>
2020-01-12 20:37:45 +0200

    add template, new styles

--- app/js/main.js
+++ app/js/main.js
@@ -52,6 +52,63 @@ $(document).ready(function () {
 	$(".form-inner-item.phone input").inputmask({"mask": "+7(999) 999-9999"});
 	$(".video").fancybox().attr('data-fancybox', 'group');
 
+	$(".form-validate").validate({
+		rules:{
+			name:{
+				required: true,
+				minlength: 4,
+				maxlength: 16,
+			},
+			phone:{
+				required: true,
+				minlength: 6,
+				maxlength: 16,
+			},
+		},
+		messages:{
+			name:{
+				required: "Это поле обязательно для заполнения",
+				minlength: "Логин должен быть минимум 4 символа",
+				maxlength: "Максимальное число символов - 16",
+			},
+			phone:{
+				required: "Это поле обязательно для заполнения",
+				minlength: "Пароль должен быть минимум 6 символа",
+				maxlength: "Пароль должен быть максимум 16 символов",
+			},
+		}
+	});
+
+	$('.form-validate').each( function(){
+		var form = $(this);
+		form.validate({
+			rules:{
+				name:{
+					required: true,
+					minlength: 4,
+					maxlength: 16,
+				},
+				phone:{
+					required: true,
+					minlength: 6,
+					maxlength: 16,
+				},
+			},
+			messages:{
+				name:{
+					required: "Это поле обязательно для заполнения",
+					minlength: "Логин должен быть минимум 4 символа",
+					maxlength: "Максимальное число символов - 16",
+				},
+				phone:{
+					required: "Это поле обязательно для заполнения",
+					minlength: "Пароль должен быть минимум 6 символа",
+					maxlength: "Пароль должен быть максимум 16 символов",
+				},
+			}
+		});
+	});
+
 	/**************tabs**************/
 	$('.tabs-nav a').on('click', function (event) {
 		event.preventDefault();
